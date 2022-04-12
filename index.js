dfe740cd13cb8864f4eae60030e32e0e42337917
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-12 04:53:45 +0300

    commit tree f3bb0b1b8a8ec12c770461e3bea7786dd39a4f60

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // code your solution here
+
+// function greetings(morning="hello1",evening="hello2")
+// {
+//     console.log(`in the morning ${morning} Jeff and in the eveing ${evening} `);
+// }
+// greetings()
+// greetings("hello4","hello5")
+
+// (function()
+// {
+//     console.log("HELLO JEFF");
+// });
+
+// const fn = function()
+// {
+//     console.log("Hello TJ");
+// }
+
+function saturdayFun(activity="roller-skate")
+{
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork(activity="go to the office")
+{
+    return `This Monday, I will ${activity}.`
+
+}
+
+function wrapAdjective(flair="*")
+{
+    // let result = wrapAdjective('*')
+    let emphatic = "a hard worker"
+    return function innerFunction(name="special"){
+
+        return `You are ${flair} ${emphatic} ${flair}!`
+
+
+
+    }
+    
+
+} 
+wrapAdjective()
